a9e3c70decd4a0b80190657fc1cc5778ba3d17a2
cm <14147806@qq.com>
2020-08-25 20:52:50 +0800

    add LnLogger

--- contracts/LnProxyImpl.sol
+++ contracts/LnProxyImpl.sol
@@ -12,37 +12,6 @@ contract LnProxyBase is LnAdmin {
         emit TargetUpdated(_target);
     }
 
-    function _emit(
-        bytes calldata callData,
-        uint numTopics,
-        bytes32 topic1,
-        bytes32 topic2,
-        bytes32 topic3,
-        bytes32 topic4
-    ) external onlyTarget {
-        uint size = callData.length;
-        bytes memory _callData = callData;
-
-        assembly {
-            
-            switch numTopics
-                case 0 {
-                    log0(add(_callData, 32), size)
-                }
-                case 1 {
-                    log1(add(_callData, 32), size, topic1)
-                }
-                case 2 {
-                    log2(add(_callData, 32), size, topic1, topic2)
-                }
-                case 3 {
-                    log3(add(_callData, 32), size, topic1, topic2, topic3)
-                }
-                case 4 {
-                    log4(add(_callData, 32), size, topic1, topic2, topic3, topic4)
-                }
-        }
-    }
 
     function() external payable {
         
