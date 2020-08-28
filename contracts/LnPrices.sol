4781d0aa9125257dcafc1f5016a850f13fd8d9c3
cm <14147806@qq.com>
2020-08-28 18:31:03 +0800

    add oracles contracts

---
+++ contracts/LnPrices.sol
@@ -0,0 +1,34 @@
+pragma solidity >=0.4.24;
+
+
+// a facade for prices fetch from oracles
+interface LnPrices {
+
+    // get price for a currency
+    function currentPrice(bytes32 currencyName) external view returns (uint);
+
+    // get price and updated time for a currency
+    function priceAndUpdatedTime(bytes32 currencyName) external view returns (uint price, uint time);
+
+
+    // is the price is stale
+    function isStale(bytes32 currencyName) external view returns (bool);
+
+    // the defined stale time
+    function stalePeriod() external view returns (uint);
+
+    // exchange amount of source currenty for some dest currency, also get source and dest curreny price
+    function exchange( bytes32 sourceName, uint sourceAmount, bytes32 destName ) external view returns ( uint );
+
+    // exchange amount of source currenty for some dest currency
+    function exchangeAndPrices( bytes32 sourceName, uint sourceAmount, bytes32 destName ) external view
+        returns ( uint value, uint sourceRate, uint destinationRate );
+}
+
+
+contract LnBasePrices is LnPrices{
+    // const name
+    bytes32 public constant LINA = "LINA";
+    bytes32 public constant LUSD = "lUsd";
+
+}
