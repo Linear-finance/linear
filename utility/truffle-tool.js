cb4ceaa7624694faf81440f5fe1b677b6fa8b6d9
scott <378282715@qq.com>
2020-09-01 09:29:11 +0800

    string toBytes32

--- utility/truffle-tool.js
+++ utility/truffle-tool.js
@@ -1,4 +1,7 @@
 
+const w3utils = require('web3-utils');
+const toBytes32 = key => w3utils.rightPad(w3utils.asciiToHex(key), 64);
+
 async function GetDeployed(contract) {
     try {
         var deployed = await contract.deployed();
@@ -17,5 +20,9 @@ async function DeployIfNotExist(deployer, contract) {
     return deployed;
 }
 
+
+//console.log(toBytes32(""));
+
 exports.GetDeployed = GetDeployed
 exports.DeployIfNotExist = DeployIfNotExist
+exports.toBytes32 = toBytes32
