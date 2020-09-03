ecca052380fb362c85708272c31f4fc40ea5f44f
scott <378282715@qq.com>
2020-09-03 09:24:19 +0800

    use LnAccessControl

--- contracts/LnAsset.sol
+++ contracts/LnAsset.sol
@@ -3,20 +3,24 @@ pragma solidity ^0.6.12;
 
 import "./LnErc20Handler.sol";
 import "./IAsset.sol";
+import "./LnAccessControl.sol";
+import "./LnAddressCache.sol";
 
-contract LnAsset is LnErc20Handler, LnOperatorModifier, IAsset {
-     bytes32  mKeyName;
+contract LnAsset is LnErc20Handler, IAsset {
+    bytes32  mKeyName;
 
+    // -------------------------------------------------------
+    // need set before system running value.
+    LnAddressStorage private addressStorage;
 
     function keyName() override external view returns (bytes32)
     {
         return mKeyName;
     }
 
-
-     constructor( bytes32 _key, address payable _proxy, LnTokenStorage _tokenStorage, string memory _name, string memory _symbol,
-        uint _totalSupply, uint8 _decimals, address _admin ) public 
-        LnErc20Handler(_proxy, _tokenStorage,_name,_symbol, _totalSupply, _decimals, _admin ) LnOperatorModifier(_admin) {
+    constructor( bytes32 _key, address payable _proxy, LnTokenStorage _tokenStorage, string memory _name, string memory _symbol,
+        uint _totalSupply, uint8 _decimals, address _admin, address _addrStorage) public 
+        LnErc20Handler(_proxy, _tokenStorage,_name,_symbol, _totalSupply, _decimals, _admin ) {
         
         mKeyName = _key;
         tokenStorage = _tokenStorage;
@@ -24,9 +28,28 @@ contract LnAsset is LnErc20Handler, LnOperatorModifier, IAsset {
         symbol = _symbol;
         totalSupply = _totalSupply;
         decimals = _decimals;
+        addressStorage = LnAddressStorage(_addrStorage);
     }
 
-     function _mint(address account, uint256 amount) private  {
+    // ------------------ system config ----------------------
+    function SetAddressStorage(address _address) public onlyAdmin {
+        emit UpdateAddressStorage(address(addressStorage), _address);
+        addressStorage = LnAddressStorage(_address);
+    }
+
+    // -----------------------------------------------
+    modifier OnlyIssueAssetRole(address _address) {
+        LnAccessControl accessCtrl = LnAccessControl(addressStorage.getAddress("LnAccessControl"));
+        require(accessCtrl.hasRole(accessCtrl.ISSUE_ASSET_ROLE(), _address), "Need debt system access role");
+        _;
+    }
+    modifier OnlyBurnAssetRole(address _address) {
+        LnAccessControl accessCtrl = LnAccessControl(addressStorage.getAddress("LnAccessControl"));
+        require(accessCtrl.hasRole(accessCtrl.BURN_ASSET_ROLE(), _address), "Need debt system access role");
+        _;
+    }
+
+    function _mint(address account, uint256 amount) private  {
         require(account != address(0), "ERC20: mint to the zero address");
         _beforeTokenTransfer(address(0), account, amount);
 
@@ -36,16 +59,15 @@ contract LnAsset is LnErc20Handler, LnOperatorModifier, IAsset {
         emitTransfer(address(0), account, amount);
     }
 
-    function mint(address account, uint256 amount) external onlyOperator {
+    function mint(address account, uint256 amount) external OnlyIssueAssetRole(msg.sender) {
         _mint(account, amount);
     }
 
-    function burn(address account, uint amount) external  onlyOperator {
+    function burn(address account, uint amount) external OnlyBurnAssetRole(msg.sender) {
         _burn(account, amount);
     }
 
-
-   function _burn(address account, uint256 amount) private {
+    function _burn(address account, uint256 amount) private {
         require(account != address(0), "ERC20: burn from the zero address");
         _beforeTokenTransfer(account, address(0), amount);
 
@@ -54,5 +76,6 @@ contract LnAsset is LnErc20Handler, LnOperatorModifier, IAsset {
         emitTransfer(account, address(0), amount);
     }
 
+    event UpdateAddressStorage(address oldAddr, address newAddr);
 }
 
