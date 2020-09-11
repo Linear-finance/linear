0058fe7eaacbe131e110ad1e5281c8a8943c9c64
cm <14147806@qq.com>
2020-09-11 10:59:10 +0800

    trival change

--- truffle-config.js
+++ truffle-config.js
@@ -50,7 +50,7 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     development: {
-      host: "172.24.192.1",     // Localhost (default: none)
+      host: "127.0.0.1",     // Localhost (default: none)
       port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
       gasPrice: 1
