7ae51144a2aa72c2b2712de0391699b571581042
scott <378282715@qq.com>
2020-09-18 22:56:22 +0800

    get lina Transfer log

--- online_tool/getStakingData.js
+++ online_tool/getStakingData.js
@@ -27,14 +27,11 @@ function getAbi(tokenname) {
     return abi;
 }
 
-var abiLnLinearStaking = getAbi("LnLinearStaking");
-
-const kLnLinearStaking = new ethers.Contract("0x410903Bff34f4d7DC510FbFd15E5Ba68C7218130", abiLnLinearStaking, provider);
-
-let writeTo = "stakingBlock.log";
-
 async function get_staking_logs() {
+  var abiLnLinearStaking = getAbi("LnLinearStaking");
+  const kLnLinearStaking = new ethers.Contract("0x410903Bff34f4d7DC510FbFd15E5Ba68C7218130", abiLnLinearStaking, provider);
   try {
+    let writeTo = "stakingBlock.log";
     kLnLinearStaking.on(
         "Staking", (who, value, staketime, event) => {
             let msg = '["Staking", "' + who.toString() + '", "' + value.toString() + '", "' + staketime.toString() + '", ' + event.blockNumber + '],\n';
@@ -58,7 +55,29 @@ async function get_staking_logs() {
   }
 }
 
-get_staking_logs();
+async function get_lina_transfer_logs() {
+  let abi = getAbi("LnProxyERC20");
+  const linaLnProxyERC20 = new ethers.Contract("0x3E9BC21C9b189C09dF3eF1B824798658d5011937", abi, provider);
+  try {
+    let writeTo = "lian-token.log";
+    linaLnProxyERC20.on(
+      "Transfer", (from, to, value, event) => {
+        let msg = '["Transfer", "' + from.toString() + '", "' + to.toString() + '", "' + value.toString() + '", ' + event.blockNumber + ',"' + event.transactionHash + '"],\n';
+        console.log(msg);
+        const data = new Uint8Array(Buffer.from(msg));
+        fs.appendFileSync(writeTo, data);
+      }
+    );
+    provider.polling = true; //open
+    provider.resetEventsBlock(10873716);
+  } catch (err) {
+    console.log("err :"+ err)
+  }
+}
+
+//get_staking_logs();
+
+get_lina_transfer_logs();
 
 setInterval(() => {
     //console.log('Infinite Loop Test interval n:', i++);
