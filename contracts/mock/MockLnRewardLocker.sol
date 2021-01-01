ccca452bef02206f555b7e7e38b6d2a2eae828a5
Jonathan <jonathan@linear.finance>
2021-01-01 09:02:05 +0000

    Add unit test cases for LnRewardSystem

---
+++ contracts/mock/MockLnRewardLocker.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.6;
+
+import "../interfaces/ILnRewardLocker.sol";
+
+/**
+ * @title MockLnRewardLocker
+ *
+ * @dev A mock LnRewardLocker contract for testing. We wouldn't need this
+ * contract if Hardhat supported Waffle's `calledOnContractWith` on mocks.
+ */
+contract MockLnRewardLocker is ILnRewardLocker {
+    struct AppendRewardArgs {
+        address _user;
+        uint256 _amount;
+        uint64 _lockTo;
+    }
+
+    AppendRewardArgs[] appendRewardCalls;
+
+    function lastAppendRewardCall()
+        public
+        view
+        returns (
+            address _user,
+            uint256 _amount,
+            uint64 _lockTo
+        )
+    {
+        AppendRewardArgs memory args = appendRewardCalls[appendRewardCalls.length - 1];
+        return (args._user, args._amount, args._lockTo);
+    }
+
+    function appendReward(
+        address _user,
+        uint256 _amount,
+        uint64 _lockTo
+    ) external override {
+        appendRewardCalls.push(AppendRewardArgs({_user: _user, _amount: _amount, _lockTo: _lockTo}));
+    }
+}
