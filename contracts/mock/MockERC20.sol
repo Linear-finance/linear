8416aca45d10cc83c2d230b1ae46fae9f5eb141a
Jonathan <jonathan@linear.finance>
2021-01-08 08:22:30 +0000

    Add test cases for LnErc20Bridge

--- contracts/mock/MockERC20.sol
+++ contracts/mock/MockERC20.sol
@@ -1,22 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.12;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 /**
  * @title MockERC20
  *
- * @dev A mintable ERC20 token contract for testing.
+ * @dev A mintable ERC20 token contract for testing. Anyone can mint or burn. DO NOT
+ * use it for production.
  */
-contract MockERC20 is ERC20, Ownable {
+contract MockERC20 is ERC20 {
     constructor(string memory _name, string memory _symbol) public ERC20(_name, _symbol) {}
 
-    function mint(address account, uint256 amount) external onlyOwner {
+    function mint(address account, uint256 amount) external {
         _mint(account, amount);
     }
 
-    function burn(address account, uint256 amount) external onlyOwner {
+    function burn(address account, uint256 amount) external {
         _burn(account, amount);
     }
 }
