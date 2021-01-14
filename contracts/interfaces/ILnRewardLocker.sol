637e6cc02e3501cea8177f6644f02b82af47733d
Jonathan <jonathan@linear.finance>
2021-01-14 16:57:23 +0000

    Convert LnRewardLocker refs to interface

--- contracts/interfaces/ILnRewardLocker.sol
+++ contracts/interfaces/ILnRewardLocker.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.7.6;
+pragma solidity >=0.6.12 <0.8.0;
 
 interface ILnRewardLocker {
+    function balanceOf(address user) external view returns (uint256);
+
+    function totalNeedToReward() external view returns (uint256);
+
     function appendReward(
         address _user,
         uint256 _amount,
