0d2b2eb68aa84ac1da65e9f5e47c1f485adcb919
Jonathan <jonathan@linear.finance>
2021-01-14 14:42:45 +0000

    Replace IERC20.sol with OpenZeppelin interface

--- contracts/LnSimpleStaking.sol
+++ contracts/LnSimpleStaking.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.12;
 
-import "./IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./LnAdmin.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Pausable.sol";
