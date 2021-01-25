55173c702e287ec806940e534ce2d81351e8748b
Jonathan <jonathan@linear.finance>
2021-01-25 06:04:39 +0000

    Implement fee split in LnExchangeSystem

--- contracts/LnExchangeSystem.sol
+++ contracts/LnExchangeSystem.sol
@@ -14,15 +14,29 @@ contract LnExchangeSystem is LnAdminUpgradeable, LnAddressCache {
     using SafeMath for uint;
     using SafeDecimalMath for uint;
 
-    bytes32 public constant ASSETS_KEY = ("LnAssetSystem");
-    bytes32 public constant PRICES_KEY = ("LnPrices");
-    bytes32 public constant CONFIG_KEY = ("LnConfig");
-    bytes32 public constant REWARD_SYS_KEY = ("LnRewardSystem");
+    event ExchangeAsset(
+        address fromAddr,
+        bytes32 sourceKey,
+        uint sourceAmount,
+        address destAddr,
+        bytes32 destKey,
+        uint destRecived,
+        uint feeForPool,
+        uint feeForFoundation
+    );
+    event FoundationFeeHolderChanged(address oldHolder, address newHolder);
 
     ILnAddressStorage mAssets;
     ILnPrices mPrices;
     ILnConfig mConfig;
     address mRewardSys;
+    address foundationFeeHolder;
+
+    bytes32 private constant ASSETS_KEY = "LnAssetSystem";
+    bytes32 private constant PRICES_KEY = "LnPrices";
+    bytes32 private constant CONFIG_KEY = "LnConfig";
+    bytes32 private constant REWARD_SYS_KEY = "LnRewardSystem";
+    bytes32 private constant CONFIG_FEE_SPLIT = "FoundationFeeSplit";
 
     function __LnExchangeSystem_init(address _admin) public initializer {
         __LnAdminUpgradeable_init(_admin);
@@ -38,6 +52,16 @@ contract LnExchangeSystem is LnAdminUpgradeable, LnAddressCache {
         emit CachedAddressUpdated(PRICES_KEY, address(mPrices));
         emit CachedAddressUpdated(CONFIG_KEY, address(mConfig));
         emit CachedAddressUpdated(REWARD_SYS_KEY, address(mRewardSys));
+    }
+
+    function setFoundationFeeHolder(address _foundationFeeHolder) public onlyAdmin {
+        require(_foundationFeeHolder != address(0), "LnExchangeSystem: zero address");
+        require(_foundationFeeHolder != foundationFeeHolder, "LnExchangeSystem: foundation fee holder not changed");
+
+        address oldHolder = foundationFeeHolder;
+        foundationFeeHolder = _foundationFeeHolder;
+
+        emit FoundationFeeHolderChanged(oldHolder, foundationFeeHolder);
     }
 
     function exchange(
@@ -61,41 +85,51 @@ contract LnExchangeSystem is LnAdminUpgradeable, LnAddressCache {
         uint destAmount = mPrices.exchange(sourceKey, sourceAmount, destKey);
         require(destAmount > 0, "dest amount must > 0");
 
-        // 计算手续费
-        //uint feeRate = 1 ether / 100; // 0.01
-        //  JS设置手续费:  await config.batchSet( ["BTC","CNY"].map(toBytes32), [0.01, 0.01].map(toUnit));
-        uint feeRate = mConfig.getUint(destKey); // fee rate
+        uint feeRate = mConfig.getUint(destKey);
         uint destRecived = destAmount.multiplyDecimal(SafeDecimalMath.unit().sub(feeRate));
         uint fee = destAmount.sub(destRecived);
 
-        // 把手续费转成USD
-        uint feeUsd = mPrices.exchange(destKey, fee, mPrices.LUSD());
-        // 这里接下来要把手续费放入资金池
-        _addExchangeFee(feeUsd);
+        // Fee going into the pool, to be adjusted based on foundation split
+        uint feeForPoolInUsd = mPrices.exchange(destKey, fee, mPrices.LUSD());
+
+        // Split the fee between pool and foundation when both holder and ratio are set
+        uint256 foundationSplit;
+        if (foundationFeeHolder == address(0)) {
+            foundationSplit = 0;
+        } else {
+            uint256 splitRatio = mConfig.getUint(CONFIG_FEE_SPLIT);
+
+            if (splitRatio == 0) {
+                foundationSplit = 0;
+            } else {
+                foundationSplit = feeForPoolInUsd.multiplyDecimal(splitRatio);
+                feeForPoolInUsd = feeForPoolInUsd.sub(foundationSplit);
+            }
+        }
+
+        ILnAsset lusd =
+            ILnAsset(mAssets.getAddressWithRequire(mPrices.LUSD(), "LnExchangeSystem: failed to get lUSD address"));
+
+        if (feeForPoolInUsd > 0) lusd.mint(mRewardSys, feeForPoolInUsd);
+        if (foundationSplit > 0) lusd.mint(foundationFeeHolder, foundationSplit);
 
         // 先不考虑预言机套利的问题
         source.burn(fromAddr, sourceAmount);
 
         dest.mint(destAddr, destRecived);
 
-        emit ExchangeAsset(fromAddr, sourceKey, sourceAmount, destAddr, destKey, destRecived, feeUsd);
+        emit ExchangeAsset(
+            fromAddr,
+            sourceKey,
+            sourceAmount,
+            destAddr,
+            destKey,
+            destRecived,
+            feeForPoolInUsd,
+            foundationSplit
+        );
     }
-
-    function _addExchangeFee(uint feeUsd) internal {
-        ILnAsset lusd = ILnAsset(mAssets.getAddressWithRequire(mPrices.LUSD(), ""));
-        lusd.mint(mRewardSys, feeUsd);
-    }
-
-    event ExchangeAsset(
-        address fromAddr,
-        bytes32 sourceKey,
-        uint sourceAmount,
-        address destAddr,
-        bytes32 destKey,
-        uint destRecived,
-        uint fee
-    );
 
     // Reserved storage space to allow for layout changes in the future.
-    uint256[46] private __gap;
+    uint256[45] private __gap;
 }
