ae9f374ba61e99e22b83c4417cda39f90f96717f
Jonathan <jonathan@linear.finance>
2021-02-23 07:39:50 +0000

    Add deployment script for changing price staleness period to 2 minutes

---
+++ scripts/bscmainnet/change_price_staleness_to_2_mins.ts
@@ -0,0 +1,31 @@
+import { Duration } from "luxon";
+import { ethers } from "hardhat";
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  const admin = deployer;
+
+  const bandProtocolAddress = "0xA4e2866745E115F4467923603fFDe0f73732B849";
+
+  const LnBandProtocol = await ethers.getContractFactory("LnBandProtocol", {
+    signer: deployer,
+    libraries: {
+      "contracts/SafeDecimalMath.sol:SafeDecimalMath":
+        "0xC065a00fbf75366D8D228f856D470C3A7c4D928c",
+    },
+  });
+
+  const lnBandProtocol = LnBandProtocol.attach(bandProtocolAddress);
+  await lnBandProtocol.connect(admin).setStalePeriod(
+    Duration.fromObject({
+      minutes: 2,
+    }).as("seconds") // _time
+  );
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
