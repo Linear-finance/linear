7a144aa8b7257c7b9ac8ea7eb8374930574101e9
Jonathan LEI <xJonathan@outlook.com>
2021-06-30 02:35:06 +0000

    Add basic sanity tests for perp trading

---
+++ tests/integration/Perpetual.spec.ts
@@ -0,0 +1,162 @@
+import { DateTime, Duration } from "luxon";
+import { ethers } from "hardhat";
+import { expect } from "chai";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+
+import { expandTo18Decimals, uint256Max } from "../utilities";
+import { deployLinearStack, DeployedStack } from "../utilities/init";
+import {
+  getBlockDateTime,
+  setNextBlockTimestamp,
+} from "../utilities/timeTravel";
+
+const { formatBytes32String } = ethers.utils;
+
+describe("Integration | Perpetual", function () {
+  let deployer: SignerWithAddress,
+    admin: SignerWithAddress,
+    alice: SignerWithAddress,
+    bob: SignerWithAddress;
+
+  let stack: DeployedStack;
+
+  const settlementDelay: Duration = Duration.fromObject({ minutes: 1 });
+  const revertDelay: Duration = Duration.fromObject({ minutes: 10 });
+  let priceUpdateTime: DateTime;
+
+  const passSettlementDelay = async (): Promise<void> => {
+    await setNextBlockTimestamp(
+      ethers.provider,
+      (await getBlockDateTime(ethers.provider)).plus(settlementDelay)
+    );
+  };
+
+  const setLbtcPrice = async (price: number): Promise<void> => {
+    await stack.lnPrices.connect(admin).setPrice(
+      formatBytes32String("lBTC"), // currencyKey
+      expandTo18Decimals(price) // price
+    );
+  };
+
+  beforeEach(async function () {
+    [deployer, alice, bob] = await ethers.getSigners();
+    admin = deployer;
+
+    stack = await deployLinearStack(deployer, admin);
+
+    priceUpdateTime = await getBlockDateTime(ethers.provider);
+
+    // Set settlement & revert delay
+    await stack.lnConfig.connect(admin).setUint(
+      ethers.utils.formatBytes32String("TradeSettlementDelay"), // key
+      settlementDelay.as("seconds")
+    );
+    await stack.lnConfig.connect(admin).setUint(
+      ethers.utils.formatBytes32String("TradeRevertDelay"), // key
+      revertDelay.as("seconds")
+    );
+
+    // Set LINA price to $0.1 and lBTC to $20,000
+    await stack.lnPrices.connect(admin).setPriceAndTime(
+      formatBytes32String("LINA"), // currencyKey
+      expandTo18Decimals(0.1), // price
+      priceUpdateTime.toSeconds() // updateTime
+    );
+    await stack.lnPrices.connect(admin).setPriceAndTime(
+      formatBytes32String("lBTC"), // currencyKey
+      expandTo18Decimals(20_000), // price
+      priceUpdateTime.toSeconds() // updateTime
+    );
+
+    // Mint 1,000,000 LINA to Alice
+    await stack.linaToken
+      .connect(admin)
+      .mint(alice.address, expandTo18Decimals(1_000_000));
+
+    // Alice stakes all LINA and builds 10,000 lUSD
+    await stack.linaToken
+      .connect(alice)
+      .approve(stack.lnCollateralSystem.address, uint256Max);
+    await stack.lnCollateralSystem.connect(alice).stakeAndBuild(
+      formatBytes32String("LINA"), // stakeCurrnecy
+      expandTo18Decimals(1_000_000), // stakeAmount
+      expandTo18Decimals(10_000) // buildAmount
+    );
+
+    // Alice sends 1,000 lUSD to Bob
+    await stack.lusdToken.connect(alice).transfer(
+      bob.address, // recipient
+      expandTo18Decimals(10_000) // amount
+    );
+    await stack.lusdToken.connect(bob).approve(
+      stack.lnPerpExchange.address, // spender
+      uint256Max // amount
+    );
+  });
+
+  it("long position", async () => {
+    await stack.lnPerpExchange.connect(bob).openPosition(
+      formatBytes32String("lBTC"), // underlying
+      true, // isLong
+      expandTo18Decimals(0.1), // size
+      expandTo18Decimals(2_000) // collateral
+    );
+    await passSettlementDelay();
+    await stack.lnPerpExchange.connect(alice).settleAction(1);
+
+    // Fees: 20 lUSD
+    expect(await stack.lusdToken.balanceOf(stack.lbtcPerp.address)).to.equal(
+      expandTo18Decimals(1_980)
+    );
+    expect(await stack.lbtcToken.balanceOf(stack.lbtcPerp.address)).to.equal(
+      expandTo18Decimals(0.1)
+    );
+
+    expect(await stack.lnPerpPositionToken.ownerOf(1)).to.equal(bob.address);
+    expect(await stack.lnPerpPositionToken.positionPerpAddresses(1)).to.equal(
+      stack.lbtcPerp.address
+    );
+
+    const position = await stack.lbtcPerp.positions(1);
+    expect(position.isLong).to.equal(true);
+    expect(position.debt).to.equal(expandTo18Decimals(2_000));
+    expect(position.locked).to.equal(expandTo18Decimals(0.1));
+    expect(position.collateral).to.equal(expandTo18Decimals(1_980));
+
+    expect(await stack.lbtcPerp.totalUsdDebt()).to.equal(
+      expandTo18Decimals(2_000)
+    );
+  });
+
+  it("short position", async () => {
+    await stack.lnPerpExchange.connect(bob).openPosition(
+      formatBytes32String("lBTC"), // underlying
+      false, // isLong
+      expandTo18Decimals(0.1), // size
+      expandTo18Decimals(2_000) // collateral
+    );
+    await passSettlementDelay();
+    await stack.lnPerpExchange.connect(alice).settleAction(1);
+
+    // Fees: 20 lUSD
+    expect(await stack.lusdToken.balanceOf(stack.lbtcPerp.address)).to.equal(
+      expandTo18Decimals(3_980)
+    );
+    expect(await stack.lbtcToken.balanceOf(stack.lbtcPerp.address)).to.equal(0);
+
+    expect(await stack.lnPerpPositionToken.ownerOf(1)).to.equal(bob.address);
+    expect(await stack.lnPerpPositionToken.positionPerpAddresses(1)).to.equal(
+      stack.lbtcPerp.address
+    );
+
+    const position = await stack.lbtcPerp.positions(1);
+    expect(position.isLong).to.equal(false);
+    expect(position.debt).to.equal(expandTo18Decimals(0.1));
+    expect(position.locked).to.equal(0);
+    expect(position.collateral).to.equal(expandTo18Decimals(3_980));
+
+    expect(await stack.lbtcPerp.totalUnderlyingDebt()).to.equal(
+      expandTo18Decimals(0.1)
+    );
+  });
+});
--- tests/utilities/init.ts
+++ tests/utilities/init.ts
@@ -16,6 +16,7 @@ export interface DeployedStack {
   linaToken: Contract;
   lusdToken: Contract;
   lbtcToken: Contract;
+  lbtcPerp: Contract;
   lnAccessControl: Contract;
   lnAssetSystem: Contract;
   lnBuildBurnSystem: Contract;
@@ -24,6 +25,8 @@ export interface DeployedStack {
   lnConfig: Contract;
   lnDebtSystem: Contract;
   lnExchangeSystem: Contract;
+  lnPerpExchange: Contract;
+  lnPerpPositionToken: Contract;
   lnRewardLocker: Contract;
   lnRewardSystem: Contract;
   lnLiquidation: Contract;
@@ -54,6 +57,9 @@ export const deployLinearStack = async (
     LnAssetUpgradeable,
     LnCollateralSystem,
     LnConfig,
+    LnPerpetual,
+    LnPerpExchange,
+    LnPerpPositionToken,
     LnRewardLocker,
     LnRewardSystem,
   ] = await Promise.all(
@@ -64,6 +70,9 @@ export const deployLinearStack = async (
       "LnAssetUpgradeable",
       "LnCollateralSystem",
       "LnConfig",
+      "LnPerpetual",
+      "LnPerpExchange",
+      "LnPerpPositionToken",
       "LnRewardLocker",
       "LnRewardSystem",
     ].map((contractName) => ethers.getContractFactory(contractName, deployer))
@@ -392,15 +401,78 @@ export const deployLinearStack = async (
   await lbtcToken.connect(admin).updateAddressCache(lnAssetSystem.address);
 
   /**
+   * Deploy perp position NFT
+   */
+  const lnPerpPositionToken = await upgrades.deployProxy(
+    LnPerpPositionToken,
+    [],
+    {
+      initializer: "__LnPerpPositionToken_init",
+    }
+  );
+
+  /**
+   * Create perpetual exchange
+   */
+  const lnPerpExchange = await upgrades.deployProxy(
+    LnPerpExchange,
+    [
+      lnAssetSystem.address, // _lnAssetSystem
+      lnConfig.address, // _lnConfig
+      lnPerpPositionToken.address, // _positionToken
+      lusdToken.address, // _lusdToken
+      zeroAddress, // _insuranceFundHolder
+    ],
+    {
+      initializer: "__LnPerpExchange_init",
+    }
+  );
+
+  /**
+   * Grant perpetual exchange minting/burning access
+   */
+  await lnAccessControl
+    .connect(admin)
+    .SetIssueAssetRole([lnPerpExchange.address], [true]);
+  await lnAccessControl
+    .connect(admin)
+    .SetBurnAssetRole([lnPerpExchange.address], [true]);
+  await lnPerpPositionToken.connect(admin).setMinter(lnPerpExchange.address);
+  await lnPerpPositionToken.connect(admin).setBurner(lnPerpExchange.address);
+
+  /**
+   * Create perpetual lBTC
+   */
+  const lbtcPerp = await upgrades.deployProxy(
+    LnPerpetual,
+    [
+      lnPerpExchange.address, // _exchange
+      lnPerpPositionToken.address, // _positionToken
+      lusdToken.address, // _lusdToken
+      lbtcToken.address, // _underlyingToken
+      lnPrices.address, // _lnPrices
+      expandTo18Decimals(0.1), // _minInitMargin
+      expandTo18Decimals(0.05), // _maintenanceMargin
+      expandTo18Decimals(0.01), // _feeRate
+      expandTo18Decimals(0.02), // _liquidatorRewardRatio
+      expandTo18Decimals(0.4), // _insuranceFundContributionRatio
+    ],
+    {
+      initializer: "__LnPerpetual_init",
+    }
+  );
+
+  /**
    * Register lUSD on `LnBuildBurnSystem`
    */
   await lnBuildBurnSystem.connect(admin).SetLusdTokenAddress(lusdToken.address);
 
   /**
-   * Register synth assets on `LnAssetSystem`
+   * Register synth assets and perps on `LnAssetSystem`
    */
   await lnAssetSystem.connect(admin).addAsset(lusdToken.address);
   await lnAssetSystem.connect(admin).addAsset(lbtcToken.address);
+  await lnAssetSystem.connect(admin).addPerp(lbtcPerp.address);
 
   /**
    * Register LINA on `LnCollateralSystem`
@@ -460,10 +532,16 @@ export const deployLinearStack = async (
     .connect(admin)
     .updateAddressCache(lnAssetSystem.address);
 
+  /**
+   * Set LnPerpExchange pool fee holder to LnRewardSystem
+   */
+  await lnPerpExchange.connect(admin).setPoolFeeHolder(lnRewardSystem.address);
+
   return {
     linaToken,
     lusdToken,
     lbtcToken,
+    lbtcPerp,
     lnAccessControl,
     lnAssetSystem,
     lnBuildBurnSystem,
@@ -472,6 +550,8 @@ export const deployLinearStack = async (
     lnConfig,
     lnDebtSystem,
     lnExchangeSystem,
+    lnPerpExchange,
+    lnPerpPositionToken,
     lnRewardLocker,
     lnRewardSystem,
     lnLiquidation,
