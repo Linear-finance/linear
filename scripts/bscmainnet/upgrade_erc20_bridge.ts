d06251437134819c2f2b1de2ad9a813fb9435ab6
Tommy <tommy@linear.finance>
2022-10-03 16:56:39 +0000

    Add upgrade_erc20_bridge script

---
+++ scripts/bscmainnet/upgrade_erc20_bridge.ts
@@ -0,0 +1,18 @@
+import { ethers, upgrades } from "hardhat";
+
+async function main() {
+  const LnErc20Bridge = await ethers.getContractFactory("LnErc20Bridge");
+
+  await upgrades.upgradeProxy(
+    "0xF6a9bAfBc505a4Bc25888dc6aeAc57184eb2685B",
+    LnErc20Bridge,
+    { unsafeAllowCustomTypes: true }
+  );
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
