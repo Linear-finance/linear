047233c866d43d9edec58eb995cb76cd200d29c1
Tommy <tommy@linear.finance>
2022-12-19 16:16:54 +0000

    Add switch_to_chainlink_2 script for lVET

---
+++ scripts/bscmainnet/switch_to_chainlink_2.ts
@@ -0,0 +1,71 @@
+import { Duration } from "luxon";
+import { ethers } from "hardhat";
+
+import EACAggregatorProxyAbi from "./abis/EACAggregatorProxy.json";
+
+const { formatBytes32String } = ethers.utils;
+
+const ORACLE_TYPE_CHAINLINK: number = 1;
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  const admin = deployer;
+
+  const liquidName: string = "lVET";
+  const aggregatorAddress: string =
+    "0x9f1fD2cEf7b226D555A747DA0411F93c5fe74e13";
+
+  const [LnOracleRouter] = await Promise.all(
+    ["LnOracleRouter"].map((contractName) =>
+      ethers.getContractFactory(contractName, {
+        signer: deployer,
+        libraries: {
+          "contracts/SafeDecimalMath.sol:SafeDecimalMath":
+            "0xC065a00fbf75366D8D228f856D470C3A7c4D928c",
+        },
+      })
+    )
+  );
+
+  const lnOracleRouter = LnOracleRouter.attach(
+    "0x475aa5fCdf2eAEAecE4F6E83121324cB293911AB"
+  );
+
+  // Double check to make sure we're not using the wrong aggregator
+  const aggregator = ethers.ContractFactory.getContract(
+    aggregatorAddress,
+    EACAggregatorProxyAbi
+  );
+  const aggregatorDescription: string = await aggregator
+    .connect(ethers.provider)
+    .description();
+  if (aggregatorDescription != `${liquidName.substr(1)} / USD`)
+    throw new Error("Aggregator description mismatch");
+
+  // Make sure the currency isn't already set to use Chainlink
+  const oracleSettings = await lnOracleRouter.oracleSettings(
+    formatBytes32String(liquidName)
+  );
+  if (oracleSettings.oracleType === ORACLE_TYPE_CHAINLINK)
+    throw new Error("Already using Chainlink");
+
+  console.log("Changing oracle to Chainlink...");
+  await lnOracleRouter.connect(admin).addChainlinkOracle(
+    formatBytes32String(liquidName), // currencyKey
+    aggregatorAddress, // oracleAddress
+    true // removeExisting
+  );
+
+  console.log("Setting stale period to 10 minutes...");
+  await lnOracleRouter.connect(admin).setStalePeriodOverride(
+    formatBytes32String(liquidName), // currencyKey
+    Duration.fromObject({ minutes: 10 }).as("seconds") // newStalePeriod
+  );
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
